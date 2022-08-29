c97b7ecedd0aefdffcac72b7ebf6667f7db5706b
nebeltag <nebeltag@mail.ru>
2022-08-29 21:46:37 +0300

    update from teacher

--- homework_4/js/3.js
+++ homework_4/js/3.js
@@ -13,9 +13,9 @@ function DigitsOfNumber(units, dozens, hundreds) {
  * 3. hundreds - содержит число, количество сотен в параметре функции.
  * Если функции было передано не целое положительное число, либо число в ином
  * диапазоне, нежели задано в условии, функция выводит в консоль информацию
- * об ошибке и возвращает пустой объект. * 
- * 
- * @param {*} number - Параметр соответствует задаваемому числу, которое нужно 
+ * об ошибке и возвращает пустой объект. *
+ *
+ * @param {*} number - Параметр соответствует задаваемому числу, которое нужно
  * разбить на разряды и представить их в виде объекта.
  */
 DigitsOfNumber.prototype.getDigitsOfNumber = function (number) {
@@ -27,11 +27,27 @@ DigitsOfNumber.prototype.getDigitsOfNumber = function (number) {
   if (number < 0 || number >= 1000) {
     console.error(`Число ${number} не входит в заданный диапазон!`);
   }
-  if (number % 1 != 0) {
+  if (!Number.isInteger(number)) {
     console.error(`Число ${number} не является целым!`);
   }
 }
 
 const digitsOfNumber_1 = new DigitsOfNumber(this.units, this.dozens, this.hundreds);
 
-digitsOfNumber_1.getDigitsOfNumber(12.65);
+digitsOfNumber_1.getDigitsOfNumber(589.3);
+
+// function getDigitsOfNumber(num) {
+//   if (!Number.isInteger(num) || num < 0 || num > 999) {
+//     console.error(`Wrong argument, it must be an integer in range [0, 999].`);
+//     return {};
+//   }
+
+//   return {
+//     units: num % 10,
+//     dozens: Math.trunc(num / 10) % 10,
+//     hundreds: Math.trunc(num / 100) % 10,
+//   }
+// }
+
+// console.log(getDigitsOfNumber(564));
+// console.log(getDigitsOfNumber(`text`));
