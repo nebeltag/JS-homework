efd2b846a608f6f833976364f31d44174047242e
nebeltag <nebeltag@mail.ru>
2022-09-18 11:05:29 +0300

    homework_8 updates from teacher

--- homework_8/geek-shop/js/basket.js
+++ homework_8/geek-shop/js/basket.js
@@ -5,25 +5,24 @@ const basketTotalValueEl = document.querySelector(`.basketTotalValue`);
 const basketTotalEl = document.querySelector(`.basketTotal`);
 const basketEl = document.querySelector(`.basket`);
 
-
 document.querySelector(`.cart-btn-link`).addEventListener(`click`, () => {
   basketEl.classList.toggle(`hidden`);
 });
 
 const basket = {};
 
-document.querySelector(`.catalog-products-list`).addEventListener(`click`, event => {
-  if (!event.target.closest(`.product-item-btn`)) {
-    return;
-  }
+document.querySelector(`.catalog-products-list`)
+  .addEventListener(`click`, event => {
+    if (!event.target.closest(`.product-item-btn`)) {
+      return;
+    }
+    const productItem = event.target.closest(`.product-item`);
+    const id = +productItem.dataset.id;
+    const name = productItem.dataset.name;
+    const price = +productItem.dataset.price;
 
-  const productItem = event.target.closest(`.product-item`);
-  const id = +productItem.dataset.id;
-  const name = productItem.dataset.name;
-  const price = +productItem.dataset.price;
-
-  addToCart(id, name, price);
-});
+    addToCart(id, name, price);
+  });
 
 function addToCart(id, name, price) {
   if (!(id in basket)) {
@@ -33,21 +32,20 @@ function addToCart(id, name, price) {
   basketCountEl.textContent = getTotalBasketCount().toString();
   basketTotalValueEl.textContent = getTotalBasketPrice().toFixed(2);
   renderProductInBasket(id);
-};
+}
 
 function getTotalBasketCount() {
   return Object.values(basket).reduce((acc, product) => acc + product.count, 0);
-};
+}
 
 function getTotalBasketPrice() {
   return Object.values(basket)
     .reduce((acc, product) => acc + product.count * product.price, 0);
-};
+}
 
 function renderProductInBasket(id) {
   const basketRowEl = basketEl
     .querySelector(`.basketRow[data-productid="${id}"]`);
-
 
   if (!basketRowEl) {
     renderNewProductInBasket(id);
@@ -69,9 +67,10 @@ function renderNewProductInBasket(productId) {
     </div>
     <div>$${basket[productId].price}</div>
     <div>
-      $<span class="productTotalRow">${(basket[productId].price * basket[productId].count).toFixed(2)}</span>
+      $<span class="productTotalRow">
+      ${(basket[productId].price * basket[productId].count).toFixed(2)}</span>
     </div>
   </div>
   `;
   basketTotalEl.insertAdjacentHTML("beforebegin", productRow);
-};
+}
